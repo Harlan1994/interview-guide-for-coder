c90412900afcda5736d3769978bd2f2d5af56060
Harlan1994 <1041099438@qq.com>
2018-08-18 19:20:03 +0800

    添加第一章第一部分解析

---
+++ src/com/harlan/chapter01/ReverseStack.java
@@ -0,0 +1,22 @@
+package com.harlan.chapter01;
+
+import java.util.Stack;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: harlan
+ * Date: 2018/8/18
+ * Time: 10:47
+ * Description:如何仅用递归函数和栈操作逆序一个栈
+ */
+public class ReverseStack {
+
+    // 如果可以借助一个栈
+    public void reverse(Stack<Integer> stack, Stack<Integer> resultStack) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
